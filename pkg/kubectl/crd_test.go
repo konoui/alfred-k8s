9522ced2687055ce9c9d95858961fe74a0b00568
konoui <django.thon.py@gmail.com>
2020-04-04 21:29:18 +0900

    update crd tests

--- pkg/kubectl/crd_test.go
+++ pkg/kubectl/crd_test.go
@@ -1,1 +1,141 @@
 package kubectl
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/konoui/alfred-k8s/pkg/executor"
+)
+
+var testCRDsRawData = `eniconfigs.crd.k8s.amazonaws.com   2020-03-11T12:23:16Z
+meshes.appmesh.k8s.aws             2020-03-11T12:32:15Z`
+
+var testCRDs = []*CRD{
+	&CRD{
+		Name:      "eniconfigs.crd.k8s.amazonaws.com",
+		CreatedAT: "2020-03-11T12:23:16Z",
+	},
+	&CRD{
+		Name:      "meshes.appmesh.k8s.aws",
+		CreatedAT: "2020-03-11T12:32:15Z",
+	},
+}
+
+var FakeCRDFunc = func(args ...string) (*executor.Response, error) {
+	if len(args) >= 1 {
+		if args[1] == "crd" {
+			return &executor.Response{
+				Stdout: []byte(testCRDsRawData),
+			}, nil
+		}
+	}
+	return &executor.Response{}, fmt.Errorf("match no command args")
+}
+
+func TestGetCRDs(t *testing.T) {
+	tests := []struct {
+		name         string
+		fakeExecutor executor.Executor
+		want         []*CRD
+	}{
+		{
+			name:         "list CRDs",
+			fakeExecutor: NewFakeExecutor(FakeCRDFunc),
+			want:         testCRDs,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			k := SetupKubectl(t, tt.fakeExecutor)
+			got, err := k.GetCRDs()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Errorf("+want -got\n%+v", diff)
+			}
+		})
+	}
+}
+
+const testResourceName = "pod"
+
+var (
+	testBaseAllPods = []*BaseResource{
+		&BaseResource{
+			Namespace: "test1-namespace",
+			Name:      "test1-pod",
+			Age:       "11d",
+		},
+		&BaseResource{
+			Namespace: "test2-namespace",
+			Name:      "test2-pod",
+			Age:       "11d",
+		},
+	}
+	testBasePods = []*BaseResource{
+		&BaseResource{
+			Name: "test1-pod",
+			Age:  "11d",
+		},
+		&BaseResource{
+			Name: "test2-pod",
+			Age:  "11d",
+		},
+	}
+)
+
+var FakePodResourceFunc = func(args ...string) (*executor.Response, error) {
+	if len(args) >= 1 {
+		if args[0] == "get" && args[1] == testResourceName {
+			if len(args) == 3 && args[2] == allNamespaceFlag {
+				return &executor.Response{
+					Stdout: []byte(testAllPodsRawData),
+				}, nil
+			}
+
+			return &executor.Response{
+				Stdout: []byte(testPodsRawData),
+			}, nil
+		}
+	}
+	return &executor.Response{}, fmt.Errorf("match no command args")
+}
+
+func TestPodResource(t *testing.T) {
+	tests := []struct {
+		name         string
+		fakeExecutor executor.Executor
+		all          bool
+		want         []*BaseResource
+	}{
+		{
+			name:         "list pods for base resource",
+			fakeExecutor: NewFakeExecutor(FakePodResourceFunc),
+			want:         testBasePods,
+		},
+		{
+			name:         "list pods in all namespaces for base resource",
+			fakeExecutor: NewFakeExecutor(FakePodResourceFunc),
+			all:          true,
+			want:         testBaseAllPods,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			k := SetupKubectl(t, tt.fakeExecutor)
+			got, err := k.GetSpecificResources(testResourceName, tt.all)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Errorf("+want -got\n%+v", diff)
+			}
+		})
+	}
+}
